866eb2bb662d9e68d40f1f8c6d7257304340c5e3
tudohuang <tudo.huang@gmail.com>
2023-12-16 09:42:55 +0800

    model.py

---
+++ model.py
@@ -0,0 +1,23 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class SimpleCNN(nn.Module):
+    def __init__(self, num_classes):
+        super(SimpleCNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        
+        # Assuming the input dimensions are (9, 9), the output of the last conv layer will be (9, 9) as well
+        self.fc = nn.Linear(64 * 9 * 9, num_classes)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+
+        # Flatten the output for the fully connected layer
+        x = x.view(x.size(0), -1)
+
+        x = self.fc(x)
+        return x
